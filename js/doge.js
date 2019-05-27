a084700b8cf40b508d3df0989c531af3d9652cfc
nadiralaoui1 <nadiralaoui23@gmail.com>
2019-05-27 14:19:05 +0200

    added js

---
+++ js/doge.js
@@ -0,0 +1,17 @@
+function changeText(){
+
+    var text = document.getElementById('doge').innerHTML = "Woof woof!"
+
+}
+
+function defaultText(){
+
+    var text = document.getElementById('doge').innerHTML = "Woof!"
+    
+}
+
+window.onload = function() {
+
+    document.getElementById("doge").innerHTML = "WOOF!";
+
+}

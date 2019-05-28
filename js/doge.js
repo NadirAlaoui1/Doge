3a9e0fbce533d920afa0a139f8fc4037a2a7a94b
nadiralaoui1 <nadiralaoui23@gmail.com>
2019-05-28 09:18:50 +0200

    picture onclick

--- js/doge.js
+++ js/doge.js
@@ -1,17 +1,26 @@
 function changeText(){
 
     var text = document.getElementById('doge').innerHTML = "WOOF WOOF!"
-
 }
 
 function defaultText(){
 
     var text = document.getElementById('doge').innerHTML = "WOOF!"
-    
 }
 
-window.onload = function() {
+window.onload = function(){
 
     document.getElementById("doge").innerHTML = "WOOF!";
-
 }
+var image = document.getElementById("dogepng");
+var button = document.getElementById("doge");
+
+button.onclick = function(){
+
+    if(dogepic.className == "clicked"){
+        dogepic.className = "";
+    } else{
+        dogepic.className = "clicked";
+    }
+
+};
